8895a0d93c6b564dbeeac8b3e576fa435a85c782
Zuo guoli <vodbyte@outlook.com>
2024-03-20 11:54:51 +0800

    add neotree command: open with system viewer

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -5,19 +5,31 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
+    's1n7ax/nvim-window-picker',
   },
   config = function()
     require('neo-tree').setup {
-      window = {
-        mappings = {
-          ['P'] = { 'toggle_preview', config = { use_float = false, use_image_nvim = true } },
+      filesystem = {
+        window = {
+          mappings = {
+            ['P'] = { 'toggle_preview', config = { use_float = false, use_image_nvim = true } },
+            ['O'] = 'system_open',
+          },
         },
-      },
 
-      vim.keymap.set('n', '<leader>tt', '<cmd>Neotree toggle<CR>', { desc = 'Neo-[T]ree [T]oggle' }),
-      vim.keymap.set('n', '<leader>tf', '<cmd>Neotree position=float<CR>', { desc = 'Neo-[T]ree [F]loat' }),
-      vim.keymap.set('n', '<leader>tl', '<cmd>Neotree position=left<CR>', { desc = 'Neo-[T]ree [L]eft' }),
-      vim.keymap.set('n', '<leader>tr', '<cmd>Neotree position=right<CR>', { desc = 'Neo-[T]ree [R]ight' }),
+        commands = {
+          system_open = function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            vim.fn.jobstart { 'pwsh', '-c', string.format('ii "%s"', path) }
+          end,
+        },
+
+        vim.keymap.set('n', '<leader>tt', '<cmd>Neotree toggle<CR>', { desc = 'Neo-[T]ree [T]oggle' }),
+        vim.keymap.set('n', '<leader>tf', '<cmd>Neotree position=float<CR>', { desc = 'Neo-[T]ree [F]loat' }),
+        vim.keymap.set('n', '<leader>tl', '<cmd>Neotree position=left<CR>', { desc = 'Neo-[T]ree [L]eft' }),
+        vim.keymap.set('n', '<leader>tr', '<cmd>Neotree position=right<CR>', { desc = 'Neo-[T]ree [R]ight' }),
+      },
     }
   end,
 }
